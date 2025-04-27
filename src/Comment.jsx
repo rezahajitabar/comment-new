7db982b4f4ce37b6cddc8a04b1265beeb4b35fc4
reza hajitabar <rezahajitabarchehre1996@gmail.com>
2025-04-27 10:50:31 -0700

    third commit

--- src/Comment.jsx
+++ src/Comment.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Comment({comment,addReply}) {
+  const [repyText,setReplyText]=useState('');
+  const [showReplyText,setShowReplyText]=useState(false);
+
+  const handleSubmitReply=(e)=>{
+    e.preventDefault();
+    if(repyText.trim()){
+      addReply(comment.id,repyText);
+      setReplyText('');
+      setShowReplyText(false);
+    }
+  }
+
   return (
-    <div>
-        
-       <p>{comment.name}</p>
+    <div className='comment'>
+        <h3>{comment.email}</h3>
+        <p>{comment.body}</p>
+        <button onClick={()=>setShowReplyText((s)=>!s)}>
+          {showReplyText ? "Cansel Reply" : "Reply"}
+        </button>
     </div>
   )
 }
